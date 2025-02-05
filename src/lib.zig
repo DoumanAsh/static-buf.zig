b678d17ec0d2b186d0695c026e5ae266cca6232a
DoumanAsh <douman@gmx.se>
2025-02-05 19:32:36 +0900

    Minimal interface

--- src/lib.zig
+++ src/lib.zig
@@ -3,6 +3,12 @@ pub const BufError = error {
     Overflow,
 };
 
+fn take(comptime T: type, ptr: *T) T {
+    const result = ptr.*;
+    ptr.* = undefined;
+    return result;
+}
+
 fn maxInt(comptime T: type) comptime_int {
     const info = @typeInfo(T);
     const bit_count = info.Int.bits;
@@ -34,33 +40,54 @@ pub fn Buf(comptime T: type, comptime LEN: usize) type {
             items: [LEN]T = undefined,
             cursor: cursor_type = 0,
 
-            pub fn new() @This() {
+            ///Creates new instance
+            pub inline fn new() @This() {
                 return .{
                 };
             }
 
-            pub fn capacity() cursor_type {
+            ///Returns container capacity
+            pub inline fn capacity() cursor_type {
                 return @truncate(LEN);
             }
 
+            ///Returns number of elements inside container
             pub fn len(self: *const @This()) cursor_type {
                 return self.cursor;
             }
 
+            ///Returns available capacity
             pub fn remaining(self: *const @This()) cursor_type {
                 return capacity() - self.cursor;
             }
 
+            ///Checks whether the container is empty
+            pub fn empty(self: *const @This()) bool {
+                return self.cursor == 0;
+            }
+
+            ///Returns slice of available elements
             pub fn as_slice(self: *const @This()) []const T {
                 return (&self.items)[0..self.cursor];
             }
 
-            pub fn append(self: *@This(), item: T) !void {
+            ///Appends elements to the end of array
+            pub fn push_back(self: *@This(), item: T) !void {
                 if (self.cursor >= LEN) {
                     return BufError.Overflow;
                 }
                 self.items[self.cursor] = item;
                 self.cursor = self.cursor + 1;
+            }
+
+            ///Removes the last element.
+            pub fn pop_back(self: *@This()) ?T {
+                if (self.empty()) {
+                    return null;
+                }
+
+                self.cursor -= 1;
+                return take(T, &self.items[self.cursor]);
             }
         };
     }
--- tests/buf.zig
+++ tests/buf.zig
@@ -20,30 +20,64 @@ test "create some buf" {
 
     var buffer = ZeroBuf.new();
     try testing.expect(buffer.len() == 0);
+    try testing.expect(buffer.empty());
 
     var buffer_slice = buffer.as_slice();
     try testing.expect(buffer_slice.len == 0);
 
-    buffer.append(1) catch {};
+    buffer.push_back(1) catch {};
     try testing.expect(buffer.len() == 1);
     try testing.expect(buffer.remaining() == 4);
-    buffer.append(2) catch {};
+    try testing.expect(!buffer.empty());
+
+    buffer.push_back(2) catch {};
     try testing.expect(buffer.len() == 2);
     try testing.expect(buffer.remaining() == 3);
-    buffer.append(3) catch {};
+    try testing.expect(!buffer.empty());
+
+    buffer.push_back(3) catch {};
     try testing.expect(buffer.len() == 3);
     try testing.expect(buffer.remaining() == 2);
-    buffer.append(4) catch {};
+    try testing.expect(!buffer.empty());
+
+    buffer.push_back(4) catch {};
     try testing.expect(buffer.len() == 4);
     try testing.expect(buffer.remaining() == 1);
-    buffer.append(5) catch {};
+    try testing.expect(!buffer.empty());
+
+    buffer.push_back(5) catch {};
     try testing.expect(buffer.len() == 5);
     try testing.expect(buffer.remaining() == 0);
-    buffer.append(5) catch {};
-    try std.testing.expect(buffer.append(6) == buf.BufError.Overflow);
+    try testing.expect(!buffer.empty());
+
+    buffer.push_back(5) catch {};
+    try std.testing.expect(buffer.push_back(6) == buf.BufError.Overflow);
     try testing.expect(buffer.remaining() == 0);
+    try testing.expect(!buffer.empty());
 
     buffer_slice = buffer.as_slice();
     try testing.expect(buffer_slice.len == 5);
     try testing.expect(mem.eql(u8, buffer_slice, &.{1, 2, 3, 4, 5}));
+
+    try testing.expect(buffer.pop_back() == 5);
+    try testing.expect(buffer.len() == 4);
+    try testing.expect(buffer.remaining() == 1);
+
+    try testing.expect(buffer.pop_back() == 4);
+    try testing.expect(buffer.len() == 3);
+    try testing.expect(buffer.remaining() == 2);
+
+    try testing.expect(buffer.pop_back() == 3);
+    try testing.expect(buffer.len() == 2);
+    try testing.expect(buffer.remaining() == 3);
+
+    try testing.expect(buffer.pop_back() == 2);
+    try testing.expect(buffer.len() == 1);
+    try testing.expect(buffer.remaining() == 4);
+
+    try testing.expect(buffer.pop_back() == 1);
+    try testing.expect(buffer.len() == 0);
+    try testing.expect(buffer.remaining() == 5);
+
+    try testing.expect(buffer.empty());
 }
